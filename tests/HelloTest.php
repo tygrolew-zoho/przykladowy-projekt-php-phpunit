cf2f13acde2ea737008fc901b778312a996810fe
Tygrolew <tygrolew@zoho.com>
2019-09-24 18:57:15 +0100

    Dopisanie testu klasy Hello (bez autoloadera)

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -1,10 +1,24 @@
 <?php
+include_once 'Hello.php';
+
 use PHPUnit\Framework\TestCase;
 
 final class HelloTest extends TestCase
 {
+    protected $hello;
+    
+    protected function setUp() 
+    {
+        $this->hello = new Hello(); 
+    }
+    
     public function testEasy()
     {
         $this->assertTrue(true);
+    }
+    
+    public function testSay()
+    {
+        $this->assertEquals("Hello world!", $this->hello->say());
     }
 }
